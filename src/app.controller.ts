9118064d3c319b0bcb4cf2d69f1b9640e6498bf8
Vitor Augusto Silva <v.augustosilva@gmail.com>
2019-05-24 22:05:54 -0300

    Add functionality to request info and create file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('requestFile')
+  async createFile() {
+    const mensagem = await this.appService.createFile();
+
+    return mensagem;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
+var fs = require('fs');
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async createFile() {
+    const axios = require('axios');
+
+    let res = await axios.get(
+      'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=8c1fbde808efed3618de43bb73b1b5d903f52f0d',
+    );
+
+    fs.writeFile('answer.json', JSON.stringify(res.data), function(err) {
+      if (err) throw err;
+    });
+
+    return { mensagem: 'Arquivo criado com sucesso' };
   }
 }
